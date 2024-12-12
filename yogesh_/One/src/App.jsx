cdca99a22e9f6e6d62c8895b930e83a5784cb512
Yogesh Dadas <yogeshdadas1818@gmail.com>
2024-12-13 01:20:22 +0530

    added first

---
+++ yogesh_/One/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Modal from './modal/Modal';
+import Layout from './cardLayout/Layout';
+import TestMoreInfo from './modal/TestMoreInfo';
+
+const App = () => {
+  return (
+    <div>
+      <TestMoreInfo/>
+    </div>
+  );
+}
+
+export default App;
