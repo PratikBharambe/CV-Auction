b26119a30d58ab51210e242e25d4d921b04f5469
SuraCdac <sura.cdac@gmail.com>
2024-12-17 20:15:42 +0530

    starting backend

---
+++ Back-end/Cv-Auction/src/test/java/com/cv_auction/Cv_Auction/CvAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cv_auction.Cv_Auction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvAuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
