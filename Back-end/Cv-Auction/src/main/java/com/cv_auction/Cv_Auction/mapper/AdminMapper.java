d6acc1e27ecc2aa78833eb5033d203738c0e66b1
SuraCdac <sura.cdac@gmail.com>
2024-12-22 18:05:39 +0530

    mapper modified

--- Back-end/Cv-Auction/src/main/java/com/cv_auction/Cv_Auction/mapper/AdminMapper.java
+++ Back-end/Cv-Auction/src/main/java/com/cv_auction/Cv_Auction/mapper/AdminMapper.java
@@ -6,11 +6,19 @@ import com.cv_auction.Cv_Auction.dto.AdminDto;
 public class AdminMapper {
 
     public static Admin mapToAdmin(AdminDto adminDto) {
-        return new Admin(adminDto.getAid(), adminDto.getAname(), adminDto.getApwd(), adminDto.getAemail());
+        return new Admin(
+                adminDto.getAid(),
+                adminDto.getAname(),
+                adminDto.getApwd(),
+                adminDto.getAemail());
     }
 
     public static AdminDto mapToAdminDto(Admin admin) {
-        return new AdminDto(admin.getAid(), admin.getAname(), admin.getApwd(), admin.getAemail());
+        return new AdminDto(
+                admin.getAid(),
+                admin.getAname(),
+                admin.getApwd(),
+                admin.getAemail());
     }
 
 }
