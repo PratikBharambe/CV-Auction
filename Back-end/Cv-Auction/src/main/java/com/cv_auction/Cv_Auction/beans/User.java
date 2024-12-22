7708feb312288cb60e796111db7c3dd92e1afdbe
SuraCdac <sura.cdac@gmail.com>
2024-12-22 17:39:55 +0530

    beans modified

--- Back-end/Cv-Auction/src/main/java/com/cv_auction/Cv_Auction/beans/User.java
+++ Back-end/Cv-Auction/src/main/java/com/cv_auction/Cv_Auction/beans/User.java
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-package com.cv_auction.Cv_Auction.beans;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int uid;
-    private String uname;
-    private String userRole;
-    private String upwd;
-    private String uemail;
-    private String mobNo;
-    private String panCard;
-    private String address;
-    private String bankCccNo;
-    private boolean accessStatus;
-}
-=======
 package com.cv_auction.Cv_Auction.beans;
 
 import jakarta.persistence.Entity;
@@ -53,4 +23,3 @@ public class User {
     private String bankCccNo;
     private boolean accessStatus;
 }
->>>>>>> 93b57c5a82144ced9925ba58e095c452e9b25081
